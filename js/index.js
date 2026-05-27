9cad065f19cd86fab34ec082b7dfd539e6450a34
Raetoshia Hardy <rhard32@gmail.com>
2026-05-27 15:03:05 -0400

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -19,3 +19,37 @@ for (let i = 0; i < skills.length; i++) {
 copyright.innerHTML = "@ Raetoshia Hardy " + thisYear;
 
 footer.appendChild(copyright);
+
+const messageForm = document.querySelector('form[name= "leave_message"]');
+
+messageForm.addEventListener("submit", function (event) {
+    
+    event.preventDefault();
+    const usersName = event.target.usersName.value;
+    const usersEmail = event.target.usersEmail.value;
+    const usersMessage = event.target.usersMessage.value;
+
+    console.log(usersName);
+    console.log(usersEmail);
+    console.log(usersMessage);
+
+    const messageSection = document.getElementById("messages");
+    const messageList = messageSection.querySelector("ul");
+    const newMessage = document.createElement("li");
+
+    newMessage.innerHTML = '<a href="mailto:' + usersEmail + '">' + usersName + '</a><span> says: ' + usersMessage + '</span>';
+
+    const removeButton = document.createElement("button");
+    removeButton.innerText = "Remove";
+    removeButton.type = "button";
+
+    removeButton.addEventListener("click", function(event){
+        const entry = event.target.parentNode;
+        entry.remove();
+    })
+
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+
+    messageForm.reset();
+});
